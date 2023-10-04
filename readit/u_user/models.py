799427ca2e4e4e27eedb19a8ff5402f9c808c0f1
Tashev <xaocccc@gmail.com>
2023-10-04 22:24:39 +0300

    first_name max_length=100

--- readit/u_user/models.py
+++ readit/u_user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    first_name = models.CharField(max_length=120)
+    first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=120)
     
     
