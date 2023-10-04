c17c67d34f0cf316906507e6ce4b5a87036aab8c
Tashev <xaocccc@gmail.com>
2023-10-04 22:31:52 +0300

    last_name length  100, makemigrations, migrate

--- readit/u_user/models.py
+++ readit/u_user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=100)
     
     
     
