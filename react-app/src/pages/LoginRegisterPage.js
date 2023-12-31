a337e4fe5a85cfba290a31766cf8a841034f2cf9
VictorG <vrgeorgiev06@gmail.com>
2024-01-01 01:03:42 +0200

    improved UX

--- react-app/src/pages/LoginRegisterPage.js
+++ react-app/src/pages/LoginRegisterPage.js
@@ -1,12 +1,12 @@
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Form from '../components/Form';
 import DisplayUsers from '../components/DisplayUsers';
 
-const BASE_URL = "http://127.0.0.1:8000"
+const BASE_URL = "http://127.0.0.1:8000";
+const REGISTER_URL = "create_new_user";
+const LOGIN_URL = "login_user";
 
 function LoginRegisterPage(){
-	const navigate = useNavigate();
-
 	const registerFields = [
         "username",
         "first name",
@@ -16,79 +16,10 @@ function LoginRegisterPage(){
         "confirm password",
     ];
 
-	function getCSRFToken() {
-		const cookies = document.cookie.split(';');
-		for (const cookie of cookies) {
-		  const [name, value] = cookie.trim().split('=');
-		  if (name === "csrftoken") {
-			return value;
-		  }
-		}
-		return "";
-	}
-
-	const onRegisterSubmit = async (fields, formData) => {
-		const newUser = {}
-		fields.map(field => {newUser[field.replace(/ /g, '_')] = formData.get(field)})
-		
-		let response = null;
-		try{
-			response = await fetch(`${BASE_URL}/create_new_user`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-CSRFToken": getCSRFToken()
-				},
-				body: JSON.stringify(newUser)
-			})
-		}
-		catch (error){
-			console.log(error);
-			navigate("/register-error");
-		}
-
-		if (response.status === 201)
-			navigate("/registered")
-		else
-			navigate("/register-error")
-	}
-
 	const loginFields = [
         "email", // or username
         "password"
     ];
-
-	const onLoginSubmit = async (fields, formData) => {
-		const loginUser = {};
-		fields.map(field => {loginUser[field] = formData.get(field)});
-		console.log(loginUser);
-		
-		let response = null;
-		try{
-			response = await fetch(`${BASE_URL}/login_user`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-CSRFToken": getCSRFToken()
-				},
-				body: JSON.stringify(loginUser)
-			});
-
-			const data = await response.json();
-			console.log(data);
-		}
-		catch (error){
-			console.log(error);
-			navigate("/login-error");
-		}
-
-		console.log(response);
-
-		if (response.status === 200)
-			navigate("/login-successful");
-		else
-			navigate("/login-error");
-	}
 
     return (
         <>
@@ -96,21 +27,19 @@ function LoginRegisterPage(){
     		<Routes>
 				<Route path="*" element={<h1 style={{color: "red"}}>404 PAGE NOT FOUND</h1>}/>
 				<Route path="/registered" element={<h1 style={{color: "green"}}>REGISTERED SUCCESSFULLY!</h1>}/>
-				<Route path="/register-error" element={<h1 style={{color: "red"}}>SORRY! THERE WAS A PROBLEM WITH YOUR REGISTRATION!</h1>}/>
 				<Route path="/login-successful" element={<h1 style={{color: "green"}}>LOGED IN SUCCESSFULLY!</h1>}/>
-				<Route path="/login-error" element={<h1 style={{color: "red"}}>SORRY! THERE WAS A PROBLEM WITH LOGING YOU IN!</h1>}/>
     			<Route path="/register" element={<Form formType="Register"
 													   title="Create a brand new ReadIT account"
                                               		   fields={registerFields}
 													   labelText="Already have an account?"
-                                                       logicSubmitFunc={onRegisterSubmit}
+                                                       postURL={`${BASE_URL}/${REGISTER_URL}`}
 													   goToLink="/login"
 													   linkText="Log in"/>}/>
       			<Route path="/login" element={<Form formType="Log In"
                                                     fields={loginFields}
 													title="Log in with yout ReadIT account"
 													labelText="Don't have an account?"
-                                                    logicSubmitFunc={onLoginSubmit}
+                                                    postURL={`${BASE_URL}/${LOGIN_URL}`}
 													goToLink="/register"
 													linkText="Create one"/>}/>
 				<Route path="/users" element={<DisplayUsers/>}/>
